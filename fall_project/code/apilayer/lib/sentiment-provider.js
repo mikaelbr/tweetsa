872222f3061dcab2f8c5353d27cd6d49e313f788
Mikael Brevik <mikaelbre@gmail.com>
2012-10-30 19:18:12 +0100

    Added child process for running the Sentiment CLI application

--- fall_project/code/apilayer/lib/sentiment-provider.js
+++ fall_project/code/apilayer/lib/sentiment-provider.js
@@ -3,20 +3,34 @@
  * Using an external CLI to find sentiment by given tweet. 
  */
 
+var CONFIG = require('config');
+
+
 // Constructor
-var SentimentAnalysis = module.exports = new (function () {
-    
+var SentimentAnalysis = function () {
+    if(false === (this instanceof SentimentAnalysis)) {
+      return new SentimentAnalysis();
+    }
     // Do some initialization?
+};
 
-}());
-
-// Private functions? 
-
+// Private functions?
+var _is_string = function (input) {
+  return typeof(input)=='string';
+};
 
 
 // Prototype methods.
-SentimentAnalysis.prototype.get(tweet) {
+SentimentAnalysis.prototype.get = function (tweet) {
+
+  if (!_is_string(tweet)) {
+    tweet = JSON.stringify(tweet);
+  }
+
   // Get sentiment
+  var spawn = require('child_process').spawn;
+  return spawn('python', [CONFIG.Sentiment.cliPath, tweet]).stdout;
 }
 
 
+module.exports = new SentimentAnalysis();
