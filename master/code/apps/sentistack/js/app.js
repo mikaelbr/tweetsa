0da0a0ff07cae716c46c7fef8461de1900bd5ece
Mikael Brevik <mikaelbre@gmail.com>
2013-05-18 21:20:30 +0200

    More tweets

--- master/code/apps/sentistack/js/app.js
+++ master/code/apps/sentistack/js/app.js
@@ -15,6 +15,21 @@
     bindEvents: function () {
       $('#appendQuery').on('click', $.proxy(this.appendQueryInput, this));
       $('form').on('submit', $.proxy(this.formSubmitted, this));
+      $('.hide-button').on('click', this.toggleList);
+    },
+
+    toggleList: function (e) {
+      e.preventDefault();
+      var height = 60;
+      var $field = $('.input-wrap');
+
+      if ($field.height() > height) {
+        // hide
+        $field.css('overflow', 'hidden').height(height);
+      } else {
+        $field.css('overflow', 'auto').height('auto');
+      }
+      return this;
     },
 
     reset: function () {
@@ -63,16 +78,39 @@
       });
     },
 
+    fetchMore: function (query, cb) {
+      var def = $.Deferred();
+      var that = this;
+      this.fetch(query).done(function (data) {
+        var max_id = data[data.length - 1].id;
+        query = encodeURI(query);
 
+        $.ajax({
+          dataType: "jsonp", 
+          url: that.url + query + '&lang=en&count=' + that.count + '&max_id=' + max_id,
+          error: function() {
+            def.reject();
+          }
+        }).done(function (second) {
+          if (!second) {
+            def.reject();
+          }
+          def.resolve(data.concat(second));
+        }).fail(function () {
+          def.reject();
+        });
+      });
+      return def;
+    },
 
     search: function () {
       var numQueries = this.queries.length,
           that = this,
           _n = 0;
       $.each(this.queries, function(i, query) {
-        that.fetch(query).done(function (data) {
+        that.fetchMore(query).done(function (data) {
           that.data[query] = that.constructData(data);
-
+        }).always(function() { 
           if (++_n === numQueries) {
             that.constructGraph();
           }          
@@ -86,6 +124,9 @@
           neutral: 0,
           negative: 0
       };
+
+      console.log(tweets);
+
       for(var i=0;i<tweets.length;i++) {
         var classific = tweets[i].sentiment.classification;
         if(classific === 'positive') {
@@ -137,7 +178,6 @@
 
     constructGraph: function () {
       this.constructSeries();
-
       $('#container').highcharts({
         chart: {
           type: 'column'
